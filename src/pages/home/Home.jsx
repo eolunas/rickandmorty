2161860c4bc87fda822054f26b5e4879cff1b05e
Eduardo Luna <eolunas@gmail.com>
2023-11-08 11:06:00 -0500

    [FIX] Home page - card description responsive and navigation info

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,99 +4,38 @@ import {
   Col,
   Container,
   ListGroup,
+  Nav,
   Row,
 } from "react-bootstrap";
 import portadeImg from "../../assets/Rick_and_Morty_Home.jpg";
 import titleImg from "../../assets/Rick_and_Morty.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [cardInfo, setCardInfo] = useState(0);
+
   return (
     <Container>
-      <Row
-        className="align-items-center"
-        style={{ height: "calc(100vh - 3.6rem)" }}
-      >
-        <Col
-          xs={12}
-          md={5}
-          style={{
-            borderStyle: "solid",
-            borderColor: "blue",
-            maxHeight: "100%",
-          }}
-        >
+      <Row className="align-items-start my-3">
+        <Col xs={12} md={5}>
           <Card>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                height: "300px",
-              }}
-            >
-              <Card.Img
-                src={portadeImg}
-                alt="rick and morty portade"
-                style={{ width: "100%", height: "100%", objectFit: "cover" }}
-              />
-            </div>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>
-                <strong>Genre:</strong>{" "}
-                <span>
-                  Animated sitcom, Adventure, Black comedy, Satire Science
-                  fiction.
-                </span>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Created by: </strong>
-                <span>Justin Roiland, Dan Harmon</span>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Voices of:</strong>
-                <span>Justin Roiland</span>
-                <span>Chris Parnell</span>
-                <span>Spencer Grammer</span>
-                <span>Sarah Chalke</span>
-                <span>Ian Cardoni</span>
-                <span>Harry Belden</span>
-              </ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <ListGroup.Item>
-                <strong>Composer:</strong>
-                <span>Ryan Elder</span>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Country of origin:</strong>
-                <span>United States</span>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Original language:</strong>
-                <span>English</span>
-              </ListGroup.Item>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col
-          xs={12}
-          md={7}
-          style={{
-            borderStyle: "solid",
-            borderColor: "red",
-            maxHeight: "100%",
-            overflowY: "scroll",
-          }}
-        >
-          <Card>
+            <Card.Img src={portadeImg} alt="rick and morty portade" />
             <Card.Header>
-              <Card.Img
-                variant="top"
-                src={titleImg}
-                alt="rick and morty title"
-              />
+              <Nav variant="tabs" defaultActiveKey="#genre">
+                <Nav.Item>
+                  <Nav.Link href="#genre" onClick={() => setCardInfo(0)}>
+                    Description
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#moreinfo" onClick={() => setCardInfo(1)}>
+                    More info
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
             </Card.Header>
-            <Card.Body>
-              <Card.Text>
+            {cardInfo == 0 && (
+              <Card.Body>
                 <Card.Text>
                   Rick and Morty is an American adult animated science fiction
                   sitcom created by Justin Roiland and Dan Harmon for Cartoon
@@ -111,37 +50,84 @@ const Home = () => {
                   conflicting scenarios: domestic family drama, and a
                   misanthropic grandfather dragging his grandson into hijinks.
                 </Card.Text>
-              </Card.Text>
-            </Card.Body>
-            <Accordion>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Accordion Item #1</Accordion.Header>
-                <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="2">
-                <Accordion.Header>Accordion Item #2</Accordion.Header>
-                <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
+              </Card.Body>
+            )}
+            {cardInfo == 1 && (
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Card.Title>Genre</Card.Title>
+                  <Card.Text>
+                    Animated sitcom, Adventure, Black comedy, Satire Science
+                    fiction.
+                  </Card.Text>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Card.Title>Created by</Card.Title>
+                  <Card.Text>Justin Roiland, Dan Harmon</Card.Text>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Card.Title>Voices of</Card.Title>
+                  <Card.Text>
+                    Justin Roiland, Chris Parnell, Spencer Grammer, Sarah
+                    Chalke, Ian Cardoni, Harry Belden.
+                  </Card.Text>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Card.Title>Composer</Card.Title>
+                  <Card.Text>Ryan Elder</Card.Text>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Card.Title>Country of origin</Card.Title>
+                  <Card.Text>United States</Card.Text>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Card.Title>Original language</Card.Title>
+                  <Card.Text>English</Card.Text>
+                </ListGroup.Item>
+              </ListGroup>
+            )}
           </Card>
+        </Col>
+        <Col xs={12} md={7}>
+          <Card>
+            <img
+              src={titleImg}
+              alt="rick and morty title"
+              style={{ width: "100%", padding: "3% 10%" }}
+            />
+          </Card>
+
+          <Accordion>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Accordion Item #1</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Accordion Item #2</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </Col>
       </Row>
     </Container>
